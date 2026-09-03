95b2eb62409d5ca50d521ac70ba5f6ae140e5e13
CeisS黄 <huangcarlouise@gmail.com>
2026-09-03 18:24:38 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+  if (inputBox.value.trim() === '') {
+    alert("Type something first!");
+    return;
+  }
+
+  let li = document.createElement("li");
+  li.innerHTML = inputBox.value;
+  
+  let span = document.createElement("span");
+  span.innerHTML = "\u00d7"; 
+  li.appendChild(span);
+
+  listContainer.appendChild(li);
+  inputBox.value = "";
+  saveData();
+}
+
+inputBox.addEventListener("keypress", function(e) {
+  if (e.key === "Enter") {
+    addTask();
+  }
+});
+
+listContainer.addEventListener("click", function(e) {
+  if (e.target.tagName === "LI") {
+    e.target.classList.toggle("checked");
+    saveData();
+  } else if (e.target.tagName === "SPAN") {
+    e.target.parentElement.remove();
+    saveData();
+  }
+}, false);
+
+function saveData() {
+  localStorage.setItem("todoData", listContainer.innerHTML);
+}
+
+function showTask() {
+  listContainer.innerHTML = localStorage.getItem("todoData") || "";
+}
+
+showTask();
